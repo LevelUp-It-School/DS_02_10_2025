636ce6ed0a17e25f007f9320062fbcb8561b0bc7
Anton Zubochenko <zubochenko@gmail.com>
2025-10-09 21:17:54 +0300

    python lesson

---
+++ python/data_types.py
@@ -0,0 +1,111 @@
+#print("""Hello world!""")
+#Программа ввода и вывода
+# phrase = input("Введите слово: ") #или что-то написать
+# print(phrase)
+
+#PEP8
+#name
+#user_input
+#value
+#first_value
+
+# name = "Пользователь"
+# # print("Добрый день,", name, "!!!")
+# print(f"Добрый день, {name}!!!")
+
+#\n
+#\t
+
+
+# print("Сложно" + "подчиненный")
+# print("сложно " * 10)
+
+# a = 2
+# b = 2.0
+# print(type(a))
+# print(type(b))
+
+# x = 100.0
+# print(x * 5)
+
+# n_1 = "1"
+# n_2 = "2"
+# print(n_1 + n_2)
+# n_1 = int(n_1)
+# n_2 = int(n_2)
+# print(n_1 + n_2)
+
+# width = '3.7'
+# height = '2.5'
+# s = float(width) * float(height)
+# print(s)
+
+# phrase = input("Введите слово: ")
+# print(phrase)
+# print(type(phrase))
+
+
+# width = int(input("Введите ширину: "))
+# height = int(input("Введите высоту: "))
+# s = width * height
+# print(f"Площадь равна: {s}.")
+
+# x = 5
+# y = 10
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x ** y)
+# print(16 ** 0.5)
+
+# print (4 / 2.0)
+
+# last_digit = 1234 % 10
+# print(last_digit)
+
+# print(1234 // 10)
+
+# yesterday_temp = int(input("Введите температуру вчера: "))
+# today_temp = int(input("Введите температуру сегодня: "))
+
+# if today_temp > yesterday_temp:
+#     print("Сегодня теплее, чем вчера.")
+# elif today_temp < yesterday_temp:
+#     print("Сегодня холоднее, чем вчера.")
+# else:
+#     print("Сегодня такая же температура, как вчера")
+
+# if условие: 
+    # код, который выполнится, если условие истинно
+# elif другое условие:
+    # код, если первое условие не сработало, но это истинно
+# else:
+    # код, если ни одно из условий не сработало
+# x = 0
+# if x > 0:
+#     print("Число положительное")
+# elif x < 0:
+#     print("Число отрицательное")
+# else:
+#     print("Это ноль")
+
+
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+# x = 5
+# print(x > 0 and x < 10) #True только если ОБА условия истинны
+
+# print(x < 0 or x > 100) # True если ХОТЯ БЫ ОДНО условие истинно
+
+# print(not x == 100) # то же самое что и !=
+
+# text = "ПриВет"
+# # text = text.lower()
+# text = text.upper()
+# print(text)
